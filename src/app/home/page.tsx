d5f4610a0a79537690582ce2b478c9666ea398b3
Chamesss <chamsedin.azouz@gmail.com>
2024-03-02 02:47:58 +0100

    Project init

---
+++ src/app/home/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div className="mt-10 text-center">
+      <p>header = transparent</p>
+      <p>slider + search</p>
+      <p>into</p>
+      <p>fait a main</p>
+      <p>sport & loisir</p>
+      <p>guide touristique</p>
+    </div>
+  );
+}
