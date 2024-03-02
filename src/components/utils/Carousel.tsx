a3d631724b909cc929acdccdfa350c332c3bf525
Chamesss <chamsedin.azouz@gmail.com>
2024-03-03 00:50:05 +0100

    Carousel implementation

--- src/components/utils/Carousel.tsx
+++ src/components/utils/Carousel.tsx
@@ -1,78 +1,36 @@
 import Image from "next/image";
-import React, { useEffect, useRef, useState } from "react";
+import Slider from "react-slick";
 
-export default function Carousel({
-  data,
-}: {
-  data: {
-    image: string;
-  }[];
-}) {
-  // State and Ref initialization
-  const [currentImg, setCurrentImg] = useState(0);
-  const [carouselSize, setCarouselSize] = useState({ width: 0, height: 0 });
-  const carouselRef = useRef(null);
+export function ArrowNull() {
+  return null;
+}
 
-  // useEffect to get the initial carousel size
-  useEffect(() => {
-    let elem = carouselRef.current as unknown as HTMLDivElement;
-    let { width, height } = elem.getBoundingClientRect();
-    if (carouselRef.current) {
-      setCarouselSize({
-        width,
-        height,
-      });
-    }
-  }, []);
-
+export default function SimpleSlider({ data }: { data: { image: string }[] }) {
+  let settings = {
+    dots: false,
+    fade: true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    waitForAnimate: false,
+    pauseOnHover: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: <ArrowNull />,
+    prevArrow: <ArrowNull />,
+  };
   return (
-    <div className="p-4">
-      {/* Carousel container */}
-      <div className="w-[100%] h-[600px] rounded-md overflow-hidden relative items-center">
-        {/* Image container */}
-        <div
-          ref={carouselRef}
-          style={{
-            left: -currentImg * carouselSize.width,
-          }}
-          className="w-full h-full absolute flex transition-all duration-300"
-        >
-          {/* Map through data to render images */}
-          {data.map((v, i) => (
-            <div key={i} className="relative shrink-0 w-full">
-              <Image
-                className="pointer-events-none w-full"
-                alt={`carousel-image-${i}`}
-                src={v.image || "https://random.imagecdn.app/500/500"}
-                width={1000}
-                height={1000}
-              />
-            </div>
-          ))}
+    <Slider {...settings}>
+      {data.map((img, i) => (
+        <div key={i} className=" h-[700px] overflow-hidden">
+          <img
+            src={img.image}
+            alt={`${img.image}`}
+            className="w-full h-full overflow-hidden object-cover"
+          />
         </div>
-      </div>
-
-      {/* Navigation buttons */}
-      <div className="flex justify-center mt-3">
-        <button
-          disabled={currentImg === 0}
-          onClick={() => setCurrentImg((prev) => prev - 1)}
-          className={`border px-4 py-2 font-bold ${
-            currentImg === 0 && "opacity-50"
-          }`}
-        >
-          {"<"}
-        </button>
-        <button
-          disabled={currentImg === data.length - 1}
-          onClick={() => setCurrentImg((prev) => prev + 1)}
-          className={`border px-4 py-2 font-bold ${
-            currentImg === data.length - 1 && "opacity-50"
-          }`}
-        >
-          {">"}
-        </button>
-      </div>
-    </div>
+      ))}
+    </Slider>
   );
 }
