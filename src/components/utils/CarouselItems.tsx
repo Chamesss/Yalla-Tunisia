db71ab87b0dcb2f0e490fd017eac6da226dc0704
Chamesss <chamsedin.azouz@gmail.com>
2024-03-09 12:20:54 +0100

    carousel arrows

--- src/components/utils/CarouselItems.tsx
+++ src/components/utils/CarouselItems.tsx
@@ -2,34 +2,33 @@
 
 import Slider from "react-slick";
 import CardItem from "./CardItem";
-import { Key, MouseEventHandler } from "react";
-import CarouselLeftArrow from "../icons/CarouselLeftArrow";
-import CarouselRightArrow from "../icons/CarouselRightArrow";
 import { Button } from "@nextui-org/react";
 import IconArrowRight from "../icons/RightArrow";
 
-function SampleNextArrow(props: {
-  onClick?: MouseEventHandler<HTMLDivElement>;
-}) {
+function SampleNextArrow(props: any) {
   return (
     <Button
-      className="absolute top-[50%] right-0 z-10 translate-x-4 cursor-pointer"
+      className="absolute top-[50%] right-0 z-10 translate-x-4 cursor-pointer min-w-unit-10"
       onClick={props.onClick}
+      size="sm"
+      variant="flat"
     >
       <IconArrowRight fill="#707070" width={20} height={20} />
     </Button>
   );
 }
 
-function SamplePrevArrow(props: {
-  onClick?: MouseEventHandler<HTMLDivElement>;
-}) {
+function SamplePrevArrow(props: any) {
   return (
     <Button
-      className="absolute top-[50%] left-0 z-10 -translate-x-4 cursor-pointer px-0"
+      className="absolute top-[50%] left-0 z-10 -translate-x-4 cursor-pointer min-w-unit-10"
       onClick={props.onClick}
+      size="sm"
+      variant="flat"
     >
-      <CarouselLeftArrow fill="#707070" width={20} height={20} />
+      <div className="rotate-180">
+        <IconArrowRight fill="#707070" width={20} height={20} />
+      </div>
     </Button>
   );
 }
@@ -67,7 +66,7 @@ export default function SimpleSlider({ data }: any) {
   return (
     <div className="relative">
       <Slider {...settings} className="gap-4">
-        {data.map((d: any, i: Key) => (
+        {data.map((d: any, i: number) => (
           <CardItem key={i} data={d} />
         ))}
       </Slider>
