a5f4ae888eaab37874f25deff8548e661b41e4d6
Chamesss <chamsedin.azouz@gmail.com>
2024-03-12 18:06:48 +0100

    dark mode mounting issues fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { ThemeSwitcher } from "./utils/ToggleDarkMode";
 import HeaderDrawer from "./utils/Drawer";
 import DropDownItem from "./utils/DropDownItem";
@@ -8,6 +8,7 @@ import { getCategories } from "@/lib/getLandingData";
 import { CategoryType } from "@/Types";
 import { Tours } from "./utils/Constants";
 import { Button } from "@nextui-org/react";
+import { useTheme } from "next-themes";
 
 export default function Header() {
   // const authenticatedNavigationItems = [
@@ -27,7 +28,12 @@ export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const [categories, setCategories] = useState<CategoryType[] | []>([]);
   const [loading, setLoading] = useState(true);
+  const [mounted, setMounted] = useState(false);
 
+  const { resolvedTheme } = useTheme();
+  useEffect(() => {
+    setMounted(true);
+  }, []);
   useEffect(() => {
     (async () => {
       getCategories()
@@ -51,23 +57,29 @@ export default function Header() {
   return (
     <header
       id="navbar"
-      className="bg-white dark:bg-[#293749] fixed w-full z-50 top-0"
+      className="bg-white dark:bg-[#000000] fixed w-full z-50 top-0"
     >
       <nav
         className={`flex flex-row transition-all duration-500 ease-in-out justify-between items-center w-[92%]  mx-auto ${
-          isHeaderFixed ? "py-0" : "py-2"
+          isHeaderFixed ? "py-3" : "py-5"
         }`}
       >
         <div className="flex flex-row items-center gap-4">
           <HeaderDrawer isOpen={isOpen} setIsOpen={setIsOpen} />
-          <img
-            className="w-16 cursor-pointer"
-            src="https://cdn-icons-png.flaticon.com/512/5968/5968204.png"
-            alt="..."
-          />
+          {mounted && (
+            <img
+              className="w-16 cursor-pointer"
+              src={
+                resolvedTheme === "dark"
+                  ? "https://creatella.ventures/wp-content/uploads/2021/10/282-2824123_stripe-logo-png-stripe-logo-white-transparent-clipart.png"
+                  : "https://upload.wikimedia.org/wikipedia/commons/b/ba/Stripe_Logo%2C_revised_2016.svg"
+              }
+              alt="..."
+            />
+          )}
         </div>
         <div
-          className={`bg-white dark:bg-[#293749] nav-links transition-bottom absolute duration-500 ease-in-out md:static md:min-h-fit min-h-[60vh] left-0 md:w-auto  w-full flex items-center px-5 bottom-20`}
+          className={`bg-white dark:bg-black nav-links transition-bottom absolute duration-500 ease-in-out md:static md:min-h-fit min-h-[60vh] left-0 md:w-auto  w-full flex items-center px-5 bottom-20`}
         >
           <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
             <li>
