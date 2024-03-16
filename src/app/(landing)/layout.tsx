b642ea6cbc86e7e4ed7f1abc287fa74650faecce
Chamesss <chamsedin.azouz@gmail.com>
2024-03-16 21:45:05 +0100

    deployment

--- src/app/(landing)/layout.tsx
+++ src/app/(landing)/layout.tsx
@@ -6,17 +6,18 @@ export default function layout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  if (process.env.NEXT_PUBLIC_API_URL) {
-    return (
-      <div>
-        <div className="lg:mb-[8.75rem] md:mb-[9.375rem] mb-[6.25rem]">
-          <Header />
-        </div>
-        {children}
-        <Footer />
-      </div>
-    );
-  } else {
-    return <p>app</p>;
-  }
+  // if (process.env.NEXT_PUBLIC_API_URL) {
+  //   return (
+  //     <div>
+  //       <div className="lg:mb-[8.75rem] md:mb-[9.375rem] mb-[6.25rem]">
+  //         <Header />
+  //       </div>
+  //       {children}
+  //       <Footer />
+  //     </div>
+  //   );
+  // } else {
+  //   return <p>app</p>;
+  // }
+  return <p>app</p>;
 }
