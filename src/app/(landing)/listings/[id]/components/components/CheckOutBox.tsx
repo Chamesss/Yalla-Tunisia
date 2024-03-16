f966a6b20ed9b3bfd0d9d4b761cc132f41854e4e
Chamesss <chamsedin.azouz@gmail.com>
2024-03-16 17:11:29 +0100

    vercel

--- src/app/(landing)/listings/[id]/components/components/CheckOutBox.tsx
+++ src/app/(landing)/listings/[id]/components/components/CheckOutBox.tsx
@@ -1,8 +1,20 @@
+import React from "react";
+import { Autocomplete, AutocompleteItem } from "@nextui-org/react";
+import { cities } from "./cities";
+
 export default function CheckOutBox() {
   return (
     <div className="border border-solid border-black border-opacity-50 rounded-lg p-4">
       <div>
         <p>Delivered to</p>
+        <Autocomplete
+          defaultItems={cities}
+          label="Favorite Animal"
+          placeholder="Search an animal"
+          className="max-w-xs"
+        >
+          {(city) => <AutocompleteItem key={city}>{city}</AutocompleteItem>}
+        </Autocomplete>
       </div>
     </div>
   );
