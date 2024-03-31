b9851b6b801d2c527a9046907e338de163c81da5
Chamesss <chamsedin.azouz@gmail.com>
2024-03-31 23:41:55 +0100

    fightin bugs

--- src/app/(landing)/addlisting/components/AddImages.tsx
+++ src/app/(landing)/addlisting/components/AddImages.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import UploadImageIcon from "@/components/icons/UploadImageIcon";
+import TrashBin from "@/components/icons/TrashBin";
 
 interface FileChangeEvent extends React.ChangeEvent<HTMLInputElement> {
   target: HTMLInputElement & {
@@ -34,7 +35,7 @@ export default function AddImages() {
   return (
     <div>
       <h1 className="text-xl font-semibold">Pictures</h1>
-      <div className="flex flex-row overflow-auto gap-4 relative">
+      <div className="flex flex-row justify-center items-center overflow-auto gap-4 relative">
         <input
           type="file"
           id="productImages"
@@ -43,22 +44,35 @@ export default function AddImages() {
           onChange={handleImageChange}
           multiple
           required
-          className="w-20 h-44 absolute z-10 opacity-20"
+          className="w-40 h-40 absolute opacity-0"
         />
-        <label
-          htmlFor="productImages"
-          className="min-w-20 w-20 h-44 z-20 flex items-center justify-center text-center font-medium text-gray-700 bg-gray-200 rounded-md hover:bg-gray-300 cursor-pointer"
-        >
-          <UploadImageIcon className="" />
-        </label>
-        {previewImages.map((imageUrl, index) => (
-          <img
-            key={index}
-            src={imageUrl}
-            alt={`Product Preview ${index + 1}`}
-            className=" h-44"
-          />
-        ))}
+        <div className="h-full">
+          <label
+            htmlFor="productImages"
+            className="min-w-40 h-40 flex items-center justify-center font-medium text-gray-700 bg-gray-200 rounded-md hover:bg-gray-300 cursor-pointer"
+          >
+            <UploadImageIcon className="w-10 h-10" />
+          </label>
+        </div>
+        <div className="flex gap-4 h-full">
+          {previewImages.map((imageUrl, index) => (
+            <>
+              <div className="flex relative">
+                <img
+                  key={index}
+                  src={imageUrl}
+                  alt={`Product Preview ${index + 1}`}
+                  className="h-44 rounded-sm"
+                />
+                <div className="absolute z-20 w-[30%] h-full right-0 bg-black/50">
+                  <div className="flex z-30 h-full w-full items-center justify-center">
+                    <TrashBin className="text-red-500 cursor-pointer transition-all hover:-translate-y-1" />
+                  </div>
+                </div>
+              </div>
+            </>
+          ))}
+        </div>
       </div>
       {previewImages.length > 0 && (
         <span>
