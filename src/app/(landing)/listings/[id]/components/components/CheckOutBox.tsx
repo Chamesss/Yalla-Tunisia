83f4100178d032d6db770a5986387df7c4421672
Chamesss <chamsedin.azouz@gmail.com>
2024-04-17 19:16:56 +0100

    cart

--- src/app/(landing)/listings/[id]/components/components/CheckOutBox.tsx
+++ src/app/(landing)/listings/[id]/components/components/CheckOutBox.tsx
@@ -7,18 +7,26 @@ import {
   Button,
 } from "@nextui-org/react";
 import { cities } from "./cities";
-import useUserState from "@/hooks/useUserStatus";
+import { useDispatch, useSelector } from "react-redux";
+import { userState } from "@/redux/slices/userSlice";
+import { addProductToCart } from "@/redux/slices/cartSlice";
 
 type Props = {
   productId: string;
 };
 
 export default function CheckOutBox({ productId }: Props) {
-  const { isLogged } = useUserState();
-  console.log(isLogged);
+  const user: userInfoType = useSelector(userState);
+  const dispatch = useDispatch();
 
-  const addProductToCart = () => {
-    const user = useUserState();
+  const addToCart = () => {
+    if (!user.isLogged) {
+      console.log("user is not connected");
+      return;
+    } else {
+      dispatch(addProductToCart(productId));
+      console.log("added");
+    }
   };
 
   return (
@@ -41,7 +49,7 @@ export default function CheckOutBox({ productId }: Props) {
       <Divider className="my-4" />
       <div className="flex justify-center">
         <Button
-          onClick={() => addProductToCart()}
+          onClick={() => addToCart()}
           className="text-md bg-[#48b9ff] text-white dark:bg-[#3d9cd7] px-5 py-2 rounded-full hover:bg-[#41a6e5] dark:hover:bg-[#3688bc]"
         >
           Add to cart
