4e3bb475bb5d6dc8b53262241fc0a0f048daeaaf
Chamesss <chamsedin.azouz@gmail.com>
2024-04-24 01:44:33 +0100

    testing

--- src/app/(landing)/register/components/LocationPicker.tsx
+++ src/app/(landing)/register/components/LocationPicker.tsx
@@ -1,25 +1,25 @@
-import { Input } from "@nextui-org/react";
-import { useRef, useEffect, Ref, RefObject } from "react";
-export default function LocationPicker() {
-  const autoCompleteRef = useRef();
-  const inputRef = useRef<HTMLInputElement>();
-  const options = {
-    componentRestrictions: { country: "ng" },
-    fields: ["address_components", "geometry", "icon", "name"],
-    types: ["establishment"],
-  };
-  useEffect(() => {
-    if (inputRef.current) {
-      const value = new window.google.maps.places.Autocomplete(
-        inputRef.current,
-        options
-      );
-    }
-  }, []);
-  return (
-    <div>
-      <label>enter address :</label>
-      <Input ref={inputRef as Ref<HTMLInputElement>} />
-    </div>
-  );
-}
+// import { Input } from "@nextui-org/react";
+// import { useRef, useEffect, Ref, RefObject } from "react";
+// export default function LocationPicker() {
+//   const autoCompleteRef = useRef();
+//   const inputRef = useRef<HTMLInputElement>();
+//   const options = {
+//     componentRestrictions: { country: "ng" },
+//     fields: ["address_components", "geometry", "icon", "name"],
+//     types: ["establishment"],
+//   };
+//   useEffect(() => {
+//     if (inputRef.current) {
+//       autoCompleteRef.current = new window.google.maps.places.Autocomplete(
+//         inputRef.current,
+//         options
+//       );
+//     }
+//   }, []);
+//   return (
+//     <div>
+//       <label>enter address :</label>
+//       <Input ref={inputRef as Ref<HTMLInputElement>} />
+//     </div>
+//   );
+// }
