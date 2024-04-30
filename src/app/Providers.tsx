e3ae794ab6bd983c85fad6f84fe99d1216570a26
Chamesss <chamsedin.azouz@gmail.com>
2024-04-30 07:01:46 +0100

    solution

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,8 +1,12 @@
 "use client";
 import ThemeProvider from "@/providers/ThemeProvider";
 import { NextUIProvider } from "@nextui-org/react";
-import { ReduxProvider } from "@/providers/ReduxProvider";
+// import { ReduxProvider } from "@/providers/ReduxProvider";
 import FirebaseAuth from "@/middlewares/AuthStatus";
+import dynamic from "next/dynamic";
+const ReduxProviders = dynamic(() => import("@/providers/ReduxProvider"), {
+  ssr: false,
+});
 
 export default function Providers({
   children,
@@ -10,12 +14,12 @@ export default function Providers({
   children: React.ReactNode;
 }>) {
   return (
-    <ReduxProvider>
+    <ReduxProviders>
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <NextUIProvider>
           <FirebaseAuth>{children}</FirebaseAuth>
         </NextUIProvider>
       </ThemeProvider>
-    </ReduxProvider>
+    </ReduxProviders>
   );
 }
--- src/providers/ReduxProvider.tsx
+++ src/providers/ReduxProvider.tsx
@@ -2,7 +2,11 @@ import { Provider } from "react-redux";
 import { store, persistor } from "../redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
-export function ReduxProvider({ children }: { children: React.ReactNode }) {
+export default function ReduxProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
