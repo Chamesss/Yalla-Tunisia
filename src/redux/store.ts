ef63f55f32873d10985d3df190f7c73794f4142c
Chamesss <chamsedin.azouz@gmail.com>
2024-04-30 06:53:51 +0100

    persist store on client

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -8,22 +8,7 @@ import rootReducer from './rootReducer';
 // import storage from 'redux-persist/lib/storage';
 import { persistStore, persistReducer } from 'redux-persist';
 import createWebStorage from "redux-persist/lib/storage/createWebStorage";
-
-const createNoopStorage = () => {
-    return {
-        getItem(_key: any) {
-            return Promise.resolve(null);
-        },
-        setItem(_key: any, value: any) {
-            return Promise.resolve(value);
-        },
-        removeItem(_key: any) {
-            return Promise.resolve();
-        },
-    };
-};
-
-const storage = typeof window !== "undefined" ? createWebStorage("local") : createNoopStorage();
+import storage from './storage';
 
 const persistConfig = {
     key: 'root',
