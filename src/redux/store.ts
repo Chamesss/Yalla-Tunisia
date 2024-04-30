75f0f1111534c8c5929f6b5e8ff3d915c1418817
Chamesss <chamsedin.azouz@gmail.com>
2024-04-30 06:44:58 +0100

    fixing local storage

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,8 +5,25 @@ import {
     useSelector as useAppSelector,
 } from 'react-redux';
 import rootReducer from './rootReducer';
-import storage from 'redux-persist/lib/storage';
+// import storage from 'redux-persist/lib/storage';
 import { persistStore, persistReducer } from 'redux-persist';
+import createWebStorage from "redux-persist/lib/storage/createWebStorage";
+
+const createNoopStorage = () => {
+    return {
+        getItem(_key: any) {
+            return Promise.resolve(null);
+        },
+        setItem(_key: any, value: any) {
+            return Promise.resolve(value);
+        },
+        removeItem(_key: any) {
+            return Promise.resolve();
+        },
+    };
+};
+
+const storage = typeof window !== "undefined" ? createWebStorage("local") : createNoopStorage();
 
 const persistConfig = {
     key: 'root',
